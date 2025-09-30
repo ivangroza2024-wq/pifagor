ae2db4541aa844216d601e96ee8bbe3b32f28551
IvanGroza <ivangroza2000@gmail.com>
2025-10-01 01:05:56 +0300

    bot_token

--- src/main/java/com/example/pifagor/model/BotConfig.java
+++ src/main/java/com/example/pifagor/model/BotConfig.java
@@ -10,53 +10,21 @@ import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Configuration
-public class BotConfig {
 
-    private final UserService userService;
-    private final UserRepository userRepository;
-    private final FacultyRepository facultyRepository;
-    private final TelegramService telegramService;
-    private final GoogleDriveService driveService;
-    private final GroupRepository groupRepository;
-    private final RegistrationRequestService registrationRequestService;
-    private final GoogleSheetsService sheetsService;
 
-    public BotConfig(UserService userService,
-                     UserRepository userRepository,
-                     FacultyRepository facultyRepository,
-                     TelegramService telegramService,
-                     GoogleDriveService driveService,
-                     GroupRepository groupRepository,
-                     RegistrationRequestService registrationRequestService,
-                     GoogleSheetsService sheetsService) {
-        this.userService = userService;
-        this.userRepository = userRepository;
-        this.facultyRepository = facultyRepository;
-        this.telegramService = telegramService;
-        this.driveService = driveService;
-        this.groupRepository = groupRepository;
-        this.registrationRequestService = registrationRequestService;
-        this.sheetsService = sheetsService;
+    @Configuration
+    public class BotConfig {
+
+        private final MathSchoolBot bot;
+
+        public BotConfig(MathSchoolBot bot) {
+            this.bot = bot;
+        }
+
+        @Bean
+        public void registerBot() throws Exception {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(bot);
+        }
     }
 
-    @Bean
-    public MathSchoolBot mathSchoolBot() throws Exception {
-        MathSchoolBot bot = new MathSchoolBot(
-                userService,
-                userRepository,
-                facultyRepository,
-                telegramService,
-                driveService,
-                groupRepository,
-                registrationRequestService,
-                sheetsService
-        );
-
-        // Реєструємо LongPolling бота
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(bot);
-
-        return bot;
-    }
-}
