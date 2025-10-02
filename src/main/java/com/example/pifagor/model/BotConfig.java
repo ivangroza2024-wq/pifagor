baedae348fd46a9dd1e8981f86849bd8de78b23b
IvanGroza <ivangroza2000@gmail.com>
2025-10-02 23:28:36 +0300

    fix port 8080

--- src/main/java/com/example/pifagor/model/BotConfig.java
+++ src/main/java/com/example/pifagor/model/BotConfig.java
@@ -1,17 +1,12 @@
 package com.example.pifagor.model;
 
 import com.example.pifagor.bot.MathSchoolBot;
-import com.example.pifagor.repository.GroupRepository;
-import com.example.pifagor.repository.UserRepository;
-import com.example.pifagor.repository.FacultyRepository;
-import com.example.pifagor.service.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import org.telegram.telegrambots.updatesreceivers.DefaultWebhook;
 
 @Configuration
 public class BotConfig {
@@ -23,12 +18,8 @@ public class BotConfig {
 
     @Bean
     public TelegramBotsApi telegramBotsApi(MathSchoolBot bot) throws Exception {
-        DefaultWebhook defaultWebhook = new DefaultWebhook();
-        defaultWebhook.setInternalUrl("http://0.0.0.0:8080");
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class, defaultWebhook);
-
-        // Склеюємо WEBHOOK_URL + webhookPath
         SetWebhook setWebhook = SetWebhook.builder()
                 .url(externalUrl + webhookPath)
                 .build();
@@ -37,5 +28,4 @@ public class BotConfig {
 
         return botsApi;
     }
-
 }
