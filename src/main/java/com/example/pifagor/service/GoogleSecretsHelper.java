0517946ec5a6f5d11289540f666e8c4c6fc9472c
IvanGroza <ivangroza2000@gmail.com>
2025-10-08 22:26:22 +0300

    fix json web

--- src/main/java/com/example/pifagor/service/GoogleSecretsHelper.java
+++ src/main/java/com/example/pifagor/service/GoogleSecretsHelper.java
@@ -9,16 +9,17 @@ public class GoogleSecretsHelper {
         String clientSecret = System.getenv("GOOGLE_CLIENT_SECRET");
 
         String jsonContent = "{\n" +
-                "  \"installed\": {\n" +
+                "  \"web\": {\n" +
                 "    \"client_id\": \"" + clientId + "\",\n" +
-                "    \"project_id\": \"poetic-primer-472117-t2\",\n" +
+                "    \"project_id\": \"vital-victor-474511-g6\",\n" +
                 "    \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\n" +
                 "    \"token_uri\": \"https://oauth2.googleapis.com/token\",\n" +
                 "    \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\n" +
                 "    \"client_secret\": \"" + clientSecret + "\",\n" +
-                "    \"redirect_uris\": [\"http://localhost\"]\n" +
+                "    \"redirect_uris\": [\"https://pifagor.onrender.com\"]\n" +
                 "  }\n" +
                 "}";
+
 
         try (FileWriter file = new FileWriter("src/main/resources/client_secret.json")) {
             file.write(jsonContent);
