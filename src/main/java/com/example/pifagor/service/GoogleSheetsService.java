86b95be387522bfe36ecd7a6aeb06e77e4920691
IvanGroza <ivangroza2000@gmail.com>
2025-10-24 15:41:49 +0300

    fix 1

--- src/main/java/com/example/pifagor/service/GoogleSheetsService.java
+++ src/main/java/com/example/pifagor/service/GoogleSheetsService.java
@@ -241,7 +241,7 @@ public class GoogleSheetsService {
         if (rowIndex == -1) throw new Exception("Дата " + date + " не знайдена в групі " + groupName);
 
         // 4. Отримуємо заголовок (рядок студентів)
-        String headerRange = safeSheetName + "!1:1";
+        String headerRange = safeSheetName + "!2:2";
         ValueRange headerResp = sheetsService.spreadsheets().values()
                 .get(SPREADSHEET_ID, headerRange)
                 .execute();
